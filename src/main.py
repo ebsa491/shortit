6bde5437e36fabb4e0fa90646162aa589b5a66e5
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-09-13 14:33:37 +0430

    __check_args added

--- src/main.py
+++ src/main.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python
 
+import sys
+
 class ShortIt:
     """shortit main class."""
 
     def __init__(self):
-        pass
+        """ __init__ """
+
+        self.program_args = ""
+
+        self.__check_args()
+
+    def __check_args(self):
+        """
+        This method checks the program's arguments count.
+        If arguments were correct, sets self.program_args = \" \".join(sys.argv[1:]).
+        """
+
+        if len(sys.argv) > 1:
+            self.program_args = " ".join(sys.argv[1:])
+        else:
+            print("USAGE: shortit URL")
+            sys.exit(1)
 
 if __name__ == '__main__':
     app = ShortIt()
