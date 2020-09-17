b427e98c08854f5ed6f7a1b43b96f83381c9a2ea
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-09-17 13:01:02 +0430

    extract_data_from_html and get_short_url methods

--- src/api.py
+++ src/api.py
@@ -2,6 +2,7 @@
 
 import requests
 import properties
+from bs4 import BeautifulSoup
 
 class UrlAPI:
     def __init__(self):
@@ -9,6 +10,9 @@ class UrlAPI:
 
     def set_url(self, url):
         self.__long_url = url
+
+    def get_short_url(self):
+        return self.__short_url
 
     def request_short_url(self):
         
@@ -18,3 +22,9 @@ class UrlAPI:
             return -1, err
 
         return result
+
+    def extract_data_from_html(self, html_page):
+        
+        soup = BeautifulSoup(html_page, 'html.parser')
+        input_tag = soup.find("input", attrs={"id": "shortenurl"})
+        self.__short_url = input_tag.attrs["value"]
