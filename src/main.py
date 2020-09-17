73ff56b688c8d649e4068281bbd2fcf01cf3f6c9
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-09-17 13:35:15 +0430

    main() api_manager, sending request and extracting data completed

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 """Stop sending long url, short it with a command in terminal."""
 
 import sys
+import api
 
 def main():
     """The main function of the program."""
@@ -10,7 +11,31 @@ def main():
 
     if args == -1:
         print("USAGE: shortit URL")
-        sys.exit(-1)
+        sys.exit(1)
+        return
+    
+    api_manager = api.UrlAPI()
+
+    api_manager.set_url(args)
+    response_stauts, result = api_manager.request_short_url()
+
+    if response_stauts == -1:
+        print("Error in connecting to the API server...")
+        ans = input("Do you want to know the error? [Y/n] ")
+        if ans.lower() != 'n':
+            print(result)
+
+        sys.exit(1)
+        return
+    
+    api_manager.extract_data_from_html(result)
+
+    print("=========================")
+    print(api_manager.get_short_url())
+    print("=========================")
+
+    sys.exit(0)
+    return
 
 def check_args():
     """
