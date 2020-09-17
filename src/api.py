c676c40756af70dc9a74ea5e618bd974451d4b80
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-09-17 13:13:13 +0430

    docstring added to UrlAPI

--- src/api.py
+++ src/api.py
@@ -5,16 +5,22 @@ import properties
 from bs4 import BeautifulSoup
 
 class UrlAPI:
+    """A class for managing the API results."""
+
     def __init__(self):
+        """ __init__ """
         self.__long_url = ""
 
     def set_url(self, url):
+        """This method sets the self.__long_url = url (self.__long_url setter)."""
         self.__long_url = url
 
     def get_short_url(self):
+        """This method returns the self.__short_url (self.__short_url getter)."""
         return self.__short_url
 
     def request_short_url(self):
+        """This method sends a POST request to the API and returns the result text."""
         
         prarams = {properties.API_PARAM: self.__long_url}
 
@@ -26,6 +32,7 @@ class UrlAPI:
         return result
 
     def extract_data_from_html(self, html_page):
+        """This method parses the html text and finds the input tag with id=\'shortenurl\' for shorten url."""
         
         soup = BeautifulSoup(html_page, 'html.parser')
         input_tag = soup.find("input", attrs={"id": "shortenurl"})
